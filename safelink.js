1f5e0e75f80cd2136f0610ebbf63e6f0d63bbb2f
yendhai <35624051+yendhai@users.noreply.github.com>
2018-05-22 12:48:21 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -18,7 +18,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://nyobangiklan.blogspot.com/p/please-wait.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*5)+ 1);
+var xxx=null;
+if(x=="1"){xxx="2018/05/cara-mendapatkan-uang-dari-blog.html"}
+if(x=="2"){xxx="2018/05/dahjh-da.html"}
+if(x=="3"){xxx="2018/02/cuma-contoh-gambar.html"}
+if(x=="4"){xxx="go-to-url.html"}a_to_ve[i].href="https://greejack.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
